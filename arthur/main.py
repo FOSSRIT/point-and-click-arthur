32a34962390834aaef5f804b2f26790ea0b117f6
David Gay <oddshocks@gmail.com>
2012-04-16 11:14:14 -0400

    basic pygame setup

--- arthur/main.py
+++ arthur/main.py
@@ -1,3 +1,28 @@
+'''
+Main file for arthur-point-and-click
+'''
+
+import pygame, sys
 
 def main():
-    print "This game is going to rock!!!"
+
+	### INITIALIZE ###
+    pygame.init()
+
+	### WINDOW FRAME ###
+	screen = pygame.display.set_mode(640, 480) # resolution tentative
+	pygame.display.set_caption("arthur-point-and-click")
+
+	### GAME LOOP ###
+	clock = pygame.time.Clock() # framerate control for animation
+	while True
+		clock.tick(50)
+
+		# Event processing
+		for event in pygame.event.get():
+			# Quitting?
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+		# Update the screen
+		pygame.display.flip()
