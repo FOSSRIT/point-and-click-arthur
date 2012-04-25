c834e8d19bd6b54e5bc7a81e8489e47fe8bc70f7
nmb7895 <nmb7895@rit.edu>
2012-04-25 09:39:49 -0300

    added basic animation groundwork code and a helper class (does not function currently)
    - Loads image
    - Splits into frames
    
    (Don't use this one for the prototype demo)

--- prototype.py
+++ prototype.py
@@ -1,8 +1,11 @@
 import pygame, random, sys
+import os
 from pygame.locals import *
+from random import randint
 
-WINDOWWIDTH = 1200
-WINDOWHEIGHT = 900
+WINDOWWIDTH = 900
+WINDOWHEIGHT = 500
+RESOLUTION = (WINDOWWIDTH, WINDOWHEIGHT)
 
 TEXTCOLOR = (255, 255, 255)
 BACKGROUNDCOLOR = (0, 0, 0)
@@ -40,6 +43,51 @@ def drawText(text, font, surface, x, y):
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
+
+def load_sliced_sprites(w, h, filename):
+    '''
+    Specs :
+    	Master can be any height.
+    	Sprites frames width must be the same width
+    	Master width must be len(frames)*frame.width
+    '''
+    images = []
+    master_image = pygame.image.load(os.path.join('', filename)).convert_alpha()
+
+    master_width, master_height = master_image.get_size()
+    for i in xrange(int(master_width/w)):
+    	images.append(master_image.subsurface((i*w,0,w,h)))
+    return images
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, images, fps = 10):
+        pygame.sprite.Sprite.__init__(self)
+        self._images = images
+
+        # Track the time we started, and the time between updates.
+        # Then we can figure out when we have to switch the image.
+        self._start = pygame.time.get_ticks()
+        self._delay = 1000 / fps
+        self._last_update = 0
+        self._frame = 0
+
+        # Call update to set our first image.
+        self.update(pygame.time.get_ticks())
+
+    def update(self, t):
+        # Note that this doesn't work if it's been more that self._delay
+        # time between calls to update(); we only update the image once
+        # then, but it really should be updated twice.
+
+        if t - self._last_update > self._delay:
+            self._frame += 1
+            if self._frame >= len(self._images): self._frame = 0
+            self.image = self._images[self._frame]
+            self._last_update = t
+            
+    def render(self, screen):
+            self.update(pygame.time.get_ticks())
+            windowSurface.blit(self.image, self.location)
 	
 pygame.init()
 mainClock = pygame.time.Clock()
@@ -48,12 +96,20 @@ pygame.display.set_caption('Point And Prototype')
 pygame.mouse.set_visible(True)
 image = pygame.image.load("image.png")
 
+
 font = pygame.font.SysFont(None, 48)
 
 drawText('Our Prototype', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
 drawText('Press Any Key To Start', font, windowSurface, (WINDOWWIDTH / 3) - 30, (WINDOWHEIGHT / 3) + 50)
 pygame.display.update()
 waitForPlayerToPressKey()
+arthur_images = load_sliced_sprites(16, 34, 'arthur\\art\\animation.png')
+sprites	= []
+sprites.append(AnimatedSprite(arthur_images, 15))
+
+for sprite in sprites:
+    sprite.location = (100,100)
+    sprite.render(windowSurface)
 
 while (endGame ==False):
     windowSurface.fill(BACKGROUNDCOLOR)
@@ -77,7 +133,7 @@ while (endGame ==False):
         pygame.mixer.stop()
 
     #if animclicked == True:
-        #code to run animation
+        
     #elif animclicked == False:
         #code to stop animation
     
@@ -113,4 +169,3 @@ while (endGame ==False):
     
     pygame.display.update()
     
-
