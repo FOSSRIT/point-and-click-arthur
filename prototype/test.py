a65b9d66d0b51ef7d3df7873ec4d6390b3f8b8bf
Andrew Mandula <ajman@csh.rit.edu>
2012-05-11 17:02:29 -0400

    Added more basic framework to the test.py file

--- prototype/test.py
+++ prototype/test.py
@@ -1,3 +1,7 @@
+'''
+Issues:
+mouseRect issues for gui.rollover
+'''
 import pygame, random, sys
 import os
 from pygame.locals import *
@@ -8,12 +12,22 @@ WINDOWHEIGHT = 900
 
 
 class Everything:
-    windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT));
+    windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    font = pygame.font
+    TEXTCOLOR = (255, 255, 255)
     def __init__(self):
         pygame.init()
+        pygame.font.init()
+        self.font = pygame.font.SysFont(None, 48)
         mainClock = pygame.time.Clock()
         pygame.display.set_caption('Point And Prototype')
         pygame.mouse.set_visible(True)
+
+    def drawText(self, text, font, surface, x, y):
+        textobj = font.render(text, 1, everything.TEXTCOLOR)
+        textrect = textobj.get_rect()
+        textrect.topleft = (x, y)
+        surface.blit(textobj, textrect)
 
     def update(self):
         gui.update()
@@ -22,9 +36,26 @@ class Everything:
         pygame.display.update()
 
 class GUI:
+    rollover = False
+    displayText = False
+    inventory = pygame.rect
     def __init__(self):
         print("made GUI!")
+        savebox = pygame.draw.rect(everything.windowSurface,(255,255,0),(1100,0,100,50))
+        savetext = everything.drawText("Save",everything.font,everything.windowSurface,1110,10)
+        #Current issue: Rect doesnt show up, but is irrelevant when the GUI becomes sprite based
+        self.inventory = pygame.draw.rect(everything.windowSurface,(255,255,0),(300, 1000,700,180))
     def update(self):
+        if(gui.rollover == True):
+            everything.drawText("Inventory",everything.font, everything.windowSurface,300,780) 
+            self.inventory=self.inventory.move(300,820)
+        elif(gui.rollover == False):
+            self.inventory=self.inventory.move(300,1000)
+        if(gui.displayText == True):
+            textBox = pygame.draw.rect(everything.windowSurface,(255,255,0),(300,0,700,100))
+            everything.drawText("King Arthur: \"Welcome to my Game!\"",everything.font, everything.windowSurface,310,10) 
+        elif(gui.displayText == False):
+            textBox = pygame.draw.rect(everything.windowSurface,(255,255,0),(300,0,0,0))
         '''
         //save button update
         //invetory render
@@ -47,12 +78,12 @@ class Bkgrnd:
         //animations not 4 game
         '''
 class mouseEventHandler(pygame.Rect):
-	def __init__(self, mouseX, mouseY):
-		tempRect = pygame.draw.rect(everything.windowSurface,0,(mouseX,mouseY,1,1))
-	def click(self, tempRect):
-		print "mouseEvent fired!"
-		if tempRect.colliderect(tempRect):
-			return True
+    def __init__(self, mouseX, mouseY):
+        tempRect = pygame.draw.rect(everything.windowSurface,0,(mouseX,mouseY,1,1))
+    def click(self, tempRect):
+        print "mouseEvent fired!"
+        if tempRect.colliderect(tempRect):
+            return True;
 
 class Game:
     endGame = False
@@ -61,14 +92,22 @@ class Game:
         print("made Game!")
         
     def update(self):
-	testRect = pygame.draw.rect(everything.windowSurface,0,(300,100,20,20))
+        testRect = pygame.draw.rect(everything.windowSurface,0,(300,100,20,20))
         if (Game.endGame ==False):
             for event in pygame.event.get():
+                if event.type == pygame.MOUSEMOTION:
+                    mX,mY = event.pos
+                    mouseRect = pygame.draw.rect(everything.windowSurface,(0,0,0),(mX,mY,1,1))
+                    if(mouseRect.y >700):
+                        gui.rollover = True
+                    if(mouseRect.y <700):
+                        gui.rollover = False
                 if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed():
-			mX,mY = event.pos
-			mouseRect = mouseEventHandler(mX,mY)
-			if mouseRect.click(testRect):
-				print("hitMouse!")
+                    mX,mY = event.pos
+                    mouseRect = mouseEventHandler(mX,mY)
+                    if mouseRect.click(testRect):
+                        print("hitMouse!")
+                        gui.displayText = True
                 if event.type == QUIT:
                     Game.endGame = True
                 if event.type == KEYDOWN:
@@ -94,4 +133,4 @@ bkgrnd = Bkgrnd(str="kingarthur.png")
 game = Game()
 gui = GUI()
 while True:
-	everything.update()
+    everything.update()
